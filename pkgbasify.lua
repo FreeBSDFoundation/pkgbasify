2b4fb2d4162b5dfbd5b408f62e11ed25d5cfe1b4
Isaac Freund <ifreund@freebsdfoundation.org>
2025-08-19 08:05:50 +0000

    Support -PRERELEASE branches.
    Closes:		https://github.com/FreeBSDFoundation/pkgbasify/issues/32
    Sponsored by:	The FreeBSD Foundation

--- pkgbasify.lua
+++ pkgbasify.lua
@@ -63,7 +63,7 @@ local function base_repo_url()
 
 	if branch == "RELEASE" or branch:match("^BETA") or branch:match("^RC") then
 		return "pkg+https://pkg.FreeBSD.org/${ABI}/base_release_" .. minor
-	elseif branch == "CURRENT" or branch == "STABLE" then
+	elseif branch == "CURRENT" or branch == "STABLE" or branch == "PRERELEASE" then
 		return "pkg+https://pkg.FreeBSD.org/${ABI}/base_latest"
 	else
 		fatal("Unsupported FreeBSD version: " .. raw)
