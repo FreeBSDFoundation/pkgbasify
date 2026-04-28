1d367e2d639566256ccdce63082e8bee2be7d083
Isaac Freund <ifreund@freebsdfoundation.org>
2026-04-28 14:02:26 +0000

    Bump version to 1.0.0
    Sponsored by:	The FreeBSD Foundation

--- pkgbasify.lua
+++ pkgbasify.lua
@@ -15,7 +15,7 @@
 -- commit that gets tagged.
 -- Directly after the tagged commit, the version should be bumped and the
 -- "-dev" suffix added.
-local version = "1.0.0-dev"
+local version = "1.0.0"
 
 local options = {
 	create_repo_conf = true,
